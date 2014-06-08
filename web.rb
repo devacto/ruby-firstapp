8e9f039fbb3fdfe4ef39abb466937215628a1607
Victor Wibisono <devacto@gmail.com>
2014-06-08 18:36:50 +1000

    Added another call to get animals.

--- web.rb
+++ web.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require 'json'
+
 get '/' do
   "Learning Ruby on Heroku"
+end
+
+get '/animals.json' do
+  content_type :json
+  {:animal => ["elephant", "cat", "dog"]}.to_json
 end
