116c5c0098e4aad7c0869b9325ff486498765fd7
Victor Wibisono <devacto@gmail.com>
2014-06-09 12:51:31 +1000

    Added get methods for customers and orders.

--- web.rb
+++ web.rb
@@ -1,11 +1,127 @@
 require 'sinatra'
 require 'json'
 
+customers = [
+    {
+        :id => 1,
+        :joined => '2000-12-02',
+        :name => 'John',
+        :city => 'Chandler',
+        :orderTotal => 9.9956,
+        :orders => [
+            {
+                :id => 1,
+                :product => 'Shoes',
+                :total => 9.9956
+            }
+        ]
+    },
+    {
+        :id => 2,
+        :joined => '1965-01-25',
+        :name => 'Zed',
+        :city => 'Las Vegas',
+        :orderTotal => 19.99,
+        :orders => [
+            {
+                :id => 2,
+                :product => 'Baseball',
+                :total => 9.995
+            },
+            {
+                :id => 3,
+                :product => 'Bat',
+                :total => 9.995
+            }
+        ]
+    },
+    {
+        :id => 3,
+        :joined => '1944-06-15',
+        :name => 'Tina',
+        :city => 'New York',
+        :ordersTotal => 44.99,
+        :orders => [
+            {
+                :id => 4,
+                :product => 'Headphones',
+                :total => 44.99
+            }
+        ]
+    },
+    {
+        :id => 4,
+        :joined => '1995-03-28',
+        :name => 'Dave',
+        :city => 'Seattle',
+        :ordersTotal => 101.50,
+        :orders => [
+            {
+                :id => 5,
+                :product => 'Kindle',
+                :total => 101.50
+            }
+        ]
+    }
+]
+
 get '/' do
-  "Learning Ruby on Heroku"
+  send_file 'index.html'
 end
 
-get '/animals.json' do
+get '/customer' do
+  return_message = {}
+
+  if customers
+    return_message[:error] = 'false'
+    return_message[:result] = customers
+  else
+    return_message[:error] = 'true'
+    return_message[:message] = 'No customers found.'
+  end
+
   content_type :json
-  {:animals => ["elephant", "cat", "dog"]}.to_json
+  return_message.to_json
+end
+
+get '/customer/:id' do
+  return_message = {}
+  result = []
+
+  customers.each do |customer|
+    result << customer if customer[:id] == params[:id].to_i
+  end
+
+  if result.empty?
+    return_message[:error] = 'true'
+    return_message[:message] = 'Customer with that ID is not found.'
+  else
+    return_message[:error] = 'false'
+    return_message[:result] = result
+  end
+
+  content_type :json
+  return_message.to_json
+end
+
+get '/order' do
+  return_message = {}
+  result = []
+
+  customers.each do |customer|
+    customer[:orders].each do |order|
+      result << order
+    end
+  end
+
+  if result.empty?
+    return_message[:error] = 'true'
+    return_message[:message] = 'No orders found.'
+  else
+    return_message[:error] = 'false'
+    return_message[:result] = result
+  end
+
+  content_type :json
+  return_message.to_json
 end
